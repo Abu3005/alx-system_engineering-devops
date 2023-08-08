1cd76336cf64242da276454cb3b629373941edc9
Adetona Mustapha <adetona3005@gmail.com>
2023-08-09 00:15:23 +0100

    task

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,18 @@
+# There is a typ in a php file so the website returns 500
+exec { 'sed -i s@.phpp @.php @ /var/www/html/wp-settings.ph':
+  command => 'sed  -i "s@.phpp@.php@" /var/www/html/wp-settings.php',
+  path    => ['/bin','/sbin','usr/bin','usr/sbin'],
+}
+
+# There is a typo in a php file so the website returns 500
+# $string1 = "require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );"
+# $string2 = "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );"
+
+# exec { '/bin/sed -i s@.phpp @.php @ /var/www/html/wp-settings.ph':
+#  command => "/bin/sed  -i \"s@$string1@$string2@\" /var/www/html/wp-settings.php",
+#  onlyif  => 'test ! -f /var/www/html/wp-settings.php',
+# }
+
+# service { 'apache2':
+#  ensure => running,
+# }
